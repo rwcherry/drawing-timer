e99c18427aadfd81d863317174a16ebdf26d7098
Rob Cherry <rakirte@gmail.com>
2020-06-14 17:52:42 -0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DrawingTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tickTimer = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.richTextBox1.Size = new System.Drawing.Size(782, 86);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(605, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 94);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tickTimer
+            // 
+            this.tickTimer.Tick += new System.EventHandler(this.tickTimer_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 199);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 58);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Progress towards";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 369);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Drawing Timer Thing";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer tickTimer;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingTimer
+{
+    public class TimeEntry
+    {
+        public ulong milliseconds = 0;
+        public DateTime date;
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1}", this.milliseconds, this.date.ToShortDateString());
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        bool started = false;
+        ulong numMillisecondsTotal = 0; // Starting value
+        List<TimeEntry> m_entries;
+        TimeEntry thisSession;
+        ulong[] goals = {
+            10,
+            50,
+            100,
+            150,
+            250,
+            500,
+            750,
+            1000
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            m_entries = new List<TimeEntry>();
+            thisSession = new TimeEntry();
+            thisSession.date = DateTime.Today;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!started)
+            {
+                this.button1.Text = "Pause";
+                this.tickTimer.Enabled = true;
+            }
+            else
+            {
+                this.button1.Text = "Start";
+                this.tickTimer.Enabled = false;
+            }
+
+            started = !started;
+            
+        }
+
+        private void UpdateTimer()
+        {
+            TimeSpan t = TimeSpan.FromMilliseconds(numMillisecondsTotal);
+            string answer = string.Format("{0:D2}hrs {1:D2}mins {2:D2}secs",
+                                    t.Hours,
+                                    t.Minutes,
+                                    t.Seconds);
+
+            this.richTextBox1.Text = answer;
+
+            foreach (var goal in goals)
+            {
+                var millisecondsGoal = goal * 60 * 60 * 1000;
+                if (numMillisecondsTotal <= millisecondsGoal)
+                {
+                    double ratio = (double)numMillisecondsTotal / (double)millisecondsGoal;
+                    int whatever = (int)(ratio * 100);
+                    this.progressBar1.Value = whatever;
+                    this.label1.Text = string.Format("Progress Towards {0} Hours", goal);
+                    break;
+                }
+            }
+        }
+
+        private void tickTimer_Tick(object sender, EventArgs e)
+        {
+            numMillisecondsTotal += 100;
+            thisSession.milliseconds += 100;
+            UpdateTimer();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            if (File.Exists("time.data"))
+            {
+                var fileText = File.ReadAllText("time.data");
+                var lines = fileText.Split('\n');
+                foreach (string line in lines)
+                {
+                    var date_split = line.Split(',');
+                    if (date_split.Length < 2)
+                        continue;
+                    TimeEntry entry = new TimeEntry();
+                    entry.milliseconds = (ulong)Int64.Parse(date_split[0]);
+                    entry.date = Convert.ToDateTime(date_split[1]);
+                    numMillisecondsTotal += entry.milliseconds;
+                    m_entries.Add(entry);
+                }
+                UpdateTimer();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.tickTimer.Enabled = false;
+            m_entries.Add(thisSession);
+            List<string> convertList = new List<string>();
+            foreach (var entry in m_entries)
+            {
+                convertList.Add(entry.ToString());
+            }
+            File.WriteAllLines("time.data", convertList.ToArray());
+        }
+    }
+}
